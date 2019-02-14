01491aaf181dc1a1ec9777ba8f102a22240e3b96
amitkumar47 <33203319+amitkumar47@users.noreply.github.com>
2019-02-14 23:31:48 +0530

    NumberPattern3

---
+++ NumberPattern3.java
@@ -0,0 +1,38 @@
+/* Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+11
+121
+1221
+*/
+
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+       Scanner scan = new Scanner (System.in);
+		 int n = scan.nextInt();
+		 
+		 int i=1;
+		 while (i<=n) {
+			 int j=1;
+			 while (j<=i) {
+				 if (i==1) {
+					 System.out.print("1");
+				 }
+			   else if (j==1 || j==i) {
+					 System.out.print("1");
+				 }
+				 else {
+					 System.out.print("2");
+				 }
+				 j++;
+				 
+			 }
+			 System.out.println();
+			 i++;
+         }
+
+	}
+}
